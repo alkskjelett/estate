877ad1b5ae215260e8d7aa8a35e69410169c69fd
Narolsky Vladimir <alkskjelett@gmail.com>
2022-03-30 17:51:51 +0300

    update models

--- estate_app/estate/models.py
+++ estate_app/estate/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo = models.ImageField(height_field=200, width_field=200)
+    photoPath = models.CharField(max_length=255)
     alarm = models.ForeignKey("Alarm", on_delete=models.CASCADE)
 
     def __str__(self):
@@ -18,10 +18,12 @@ class Alarm(models.Model):
 class Announcement(models.Model):
     title = models.CharField(max_length=50)
     address = models.CharField(max_length=150)
+    imagePath = models.CharField(max_length=255)
     cost = models.IntegerField()
     square = models.FloatField()
+    floor = models.IntegerField()
     description = models.TextField(blank=True)
-    filters = models.TextField()
+    filters = models.ForeignKey("Filters", on_delete=models.CASCADE, blank=True)
     contacts = models.CharField(max_length=30)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
@@ -42,3 +44,23 @@ class Chat(models.Model):
 class Message(models.Model):
     message = models.TextField()
     chat = models.ForeignKey("Chat", on_delete=models.CASCADE)
+
+class Filters(models.Model):
+    appliances = models.ForeignKey("AppliancesFilters", on_delete=models.CASCADE, blank=True)
+    house = models.ForeignKey("HouseFilters", on_delete=models.CASCADE, blank=True)
+
+class AppliancesFilters(models.Model):
+    fridge = models.BooleanField(blank=True)
+    microwave = models.BooleanField(blank=True)
+    washMachine = models.BooleanField(blank=True)
+    oven = models.BooleanField(blank=True)
+    conditioner = models.BooleanField(blank=True)
+    router = models.BooleanField(blank=True)
+    TV = models.BooleanField(blank=True)
+
+class HouseFilters(models.Model):
+    type = models.CharField(max_length=20, blank=True)
+    floors = models.IntegerField(blank=True)
+    heating = models.CharField(max_length=20, blank=True)
+
+
--- estate_app/estate_app/settings.py
+++ estate_app/estate_app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'estate',
-        'PASSWORD': '12345',
+        'PASSWORD': '123',
         'PORT': '5432',
         'USER': 'postgres',
         'HOST': '127.0.0.1',
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
